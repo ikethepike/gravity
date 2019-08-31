89fe93b93a461093002d6f93d6bc4ad117b5c9cd
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2019-08-31 20:58:19 +0200

    revised structure of ws

--- js/socket.js
+++ js/socket.js
@@ -3,25 +3,29 @@ let interval = null
 
 window.pressed = false
 
-ws.onopen(() => {
+ws.onopen = () => {
   console.log('Connected!')
 
   interval = setInterval(() => {
     // Dispatch request
     ws.send('poll:request')
   }, 66)
-})
+}
 
-ws.onmessage(event => {
+ws.onmessage = event => {
   const { data } = event
 
   const input = JSON.parse(data).pressed
 
   window.pressed = Boolean(input)
-})
+}
 
-ws.onclose(() => {
+ws.onclose = () => {
   window.pressed = false
 
   clearInterval(interval)
-})
+}
+
+ws.onerror = event => {
+  console.error('Failed to connected to WS server', event)
+}
