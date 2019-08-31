0ef55e32071ae1a6d17d27cb692360522b1d3154
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2019-08-31 20:38:46 +0200

    Scaffolded out ws layer

---
+++ js/socket.js
@@ -0,0 +1,27 @@
+const ws = new WebSocket('ws://localhost:1337')
+let interval = null
+
+window.pressed = false
+
+ws.onopen(() => {
+  console.log('Connected!')
+
+  interval = setInterval(() => {
+    // Dispatch request
+    ws.send('poll:request')
+  }, 66)
+})
+
+ws.onmessage(event => {
+  const { data } = event
+
+  const input = JSON.parse(data).pressed
+
+  window.pressed = Boolean(input)
+})
+
+ws.onclose(() => {
+  window.pressed = false
+
+  clearInterval(interval)
+})
